390dbf2a13173dd69081457ab4f539615ddce040
FC <windsingcc@163.com>
2018-03-18 14:56:01 +0800

    小的修改和调试

--- src/main/java/com/fc/study/WebMvcConfig.java
+++ src/main/java/com/fc/study/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package com.fc.study;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@EnableWebMvc
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
